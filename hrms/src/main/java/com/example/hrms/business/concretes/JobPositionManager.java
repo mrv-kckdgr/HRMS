147572a064334e01be99b33048a810cec38d1a08
Kucukdogru <m42erve@hotmail.com>
2021-05-11 01:29:07 +0300

    11 Mayıs Proje Son Hali

---
+++ hrms/src/main/java/com/example/hrms/business/concretes/JobPositionManager.java
@@ -0,0 +1,26 @@
+package com.example.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.hrms.business.abstracts.JobPositionService;
+import com.example.hrms.dataAccess.abstracts.JobPositionDao;
+import com.example.hrms.entities.concretes.JobPosition;
+
+@Service
+public class JobPositionManager implements JobPositionService {
+
+	private JobPositionDao jobPositionDao;
+	
+	@Autowired
+	public JobPositionManager(JobPositionDao jobPositionDao) {
+		super();
+		this.jobPositionDao = jobPositionDao;
+	}
+	@Override
+	public List<JobPosition> getAll() {
+		return this.jobPositionDao.findAll();
+	}
+}
---
+++ hrms/src/main/java/com/example/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,43 @@
+package com.example.hrms.entities.concretes;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+
+@Entity
+@Table(name="jobpositions")
+public class JobPosition {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="position")
+	private String position;
+	
+	public JobPosition() {
+		
+	}
+	public JobPosition(int id, String position) {
+		super();
+		this.id = id;
+		this.position = position;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+}
